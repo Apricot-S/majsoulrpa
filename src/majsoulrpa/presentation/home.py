c27681656829edfa4c8becba9ba1cea9f4075546
Apricot-S <mahjong_apricot@outlook.jp>
2024-01-01 07:33:04 +0900

    fix: discard messages when the date (06:00:00 (UTC+0900)) is crossed

--- src/majsoulrpa/presentation/home.py
+++ src/majsoulrpa/presentation/home.py
@@ -274,6 +274,34 @@ class HomePresentation(PresentationBase):
 
             raise InconsistentMessage(str(message), browser.get_screenshot())
 
+    def _discard_messages_across_dates(self) -> None:
+        while True:
+            message = self._db_client.dequeue_message(0.1)
+            if message is None:
+                break
+            _, name, _, _, _ = message
+
+            match name:
+                case (
+                    ".lq.NotifyReviveCoinUpdate"
+                    | ".lq.NotifyGiftSendRefresh"
+                    | ".lq.NotifyDailyTaskUpdate"
+                    | ".lq.NotifyActivityPeriodTaskUpdate"
+                    | ".lq.NotifyShopUpdate"
+                    | ".lq.NotifyAccountChallengeTaskUpdate"
+                    | ".lq.NotifyAccountUpdate"
+                    | ".lq.Lobby.fetchShopInterval"
+                    | ".lq.Lobby.fetchActivityInterval"
+                    | ".lq.Lobby.heatbeat"
+                ):
+                    logger.info(message)
+                    continue
+                case _:
+                    raise InconsistentMessage(
+                        str(message),
+                        self._browser.get_screenshot(),
+                    )
+
     def create_room(
         self,
         mode: Literal["4-Player", "3-Player"] = "4-Player",
@@ -288,6 +316,7 @@ class HomePresentation(PresentationBase):
         self._assert_not_stale()
 
         deadline = timeout_to_deadline(timeout)
+        self._discard_messages_across_dates()
 
         # Click "Friendly Match".
         template = Template.open_file(
@@ -378,6 +407,7 @@ class HomePresentation(PresentationBase):
         self._assert_not_stale()
 
         deadline = timeout_to_deadline(timeout)
+        self._discard_messages_across_dates()
 
         # Click "Friendly Match".
         template = Template.open_file(
@@ -435,7 +465,7 @@ class HomePresentation(PresentationBase):
             time.sleep(1.0)
 
             while True:
-                message = self._db_client.dequeue_message(1.0)
+                message = self._db_client.dequeue_message(0.1)
                 if message is None:
                     break
                 logger.info(message)
