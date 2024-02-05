60f21e65759a16076a513ac53bc827f88d52b6e6
Cryolite <cryolite.indigo@gmail.com>
2024-02-05 19:54:42 +0900

    docs: add docstrings to `majsoulrpa.presentation.login`
    * add docstrings to the `majsoulrpa.presentation.login` module

--- src/majsoulrpa/presentation/login.py
+++ src/majsoulrpa/presentation/login.py
@@ -15,12 +15,40 @@ from .presentation_base import (
 
 
 class LoginPresentation(PresentationBase):
+    """Login presentation.
+
+    The `LoginPresentation` represents the first screen displayed to
+    users who are not logged in when they navigate to
+    https://game.mahjongsoul.com/ in their browser. Users can perform
+    the following operation with an instance of `LoginPresentation`:
+
+    * Click the "Login" button to transition to the `AuthPresentation`.
+    """
+
     def __init__(
         self,
         browser: BrowserBase,
         message_queue_client: MessageQueueClientBase,
         creator: PresentationCreatorBase,
     ) -> None:
+        """Creates an instance of `LoginPresentation`.
+
+        This constructor is intended to be called only within the
+        framework. Users should not directly call this constructor.
+
+        Args:
+            browser: The browser instance currently displaying the login
+                screen.
+            message_queue_client: A message queue client currently
+                connected to the queue where mitmproxy is pushing
+                messages.
+            creator: A presentation creator responsible for
+                instantiating presentations.
+
+        Raises:
+            PresentationNotDetectedError: If the login screen is not
+                detected.
+        """
         super().__init__(browser, message_queue_client, creator)
 
         template = Template.open_file(
@@ -41,6 +69,20 @@ class LoginPresentation(PresentationBase):
         template.wait_for(browser, timeout)
 
     def login(self, timeout: TimeoutType = 60.0) -> None:
+        """Clicks the "Login" button.
+
+        Initiates a transition to the `AuthPresentation` by clicking the
+        "Login" button, and waits for the authentication screen to
+        appear.
+
+        Args:
+            timeout: The maximum duration, in seconds, to wait for the
+                authentication screen to appear. Defaults to `60.0`.
+
+        Raises:
+            PresentationTimeoutError: If the authentication screen does
+                not appear within the specified timeout period.
+        """
         self._assert_not_stale()
 
         template = Template.open_file(
