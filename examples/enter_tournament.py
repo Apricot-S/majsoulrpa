57beed5babe23aabe52d631cb5cd6afc43e64939
Apricot-S <mahjong_apricot@outlook.jp>
2024-02-11 09:34:45 +0900

    docs: enable user data in examples

--- examples/enter_tournament.py
+++ examples/enter_tournament.py
@@ -22,29 +22,30 @@ if __name__ == "__main__":
     with RPA.from_config(my_config) as rpa:
         presentation = rpa.wait(timeout=20.0)
 
-        if not isinstance(presentation, LoginPresentation):
-            msg = "Could not transit to `login`."
-            raise RuntimeError(msg)
-        presentation.login(timeout=60.0)
-        if presentation.new_presentation is None:
-            msg = "Could not transit to `auth`."
-            raise RuntimeError(msg)
-        presentation = presentation.new_presentation
+        if not isinstance(presentation, HomePresentation):
+            if not isinstance(presentation, LoginPresentation):
+                msg = "Could not transit to `login`."
+                raise RuntimeError(msg)
+            presentation.login(timeout=60.0)
+            if presentation.new_presentation is None:
+                msg = "Could not transit to `auth`."
+                raise RuntimeError(msg)
+            presentation = presentation.new_presentation
 
-        if not isinstance(presentation, AuthPresentation):
-            msg = "Could not transit to `auth`."
-            raise RuntimeError(msg)
+            if not isinstance(presentation, AuthPresentation):
+                msg = "Could not transit to `auth`."
+                raise RuntimeError(msg)
 
-        auth_start_time = datetime.datetime.now(datetime.UTC)
-        login_imap = YostarLoginIMAP(my_config)
-        presentation.enter_email_address(login_imap.get_email_address())
-        auth_code = login_imap.get_auth_code(start_time=auth_start_time)
+            auth_start_time = datetime.datetime.now(datetime.UTC)
+            login_imap = YostarLoginIMAP(my_config)
+            presentation.enter_email_address(login_imap.get_email_address())
+            auth_code = login_imap.get_auth_code(start_time=auth_start_time)
 
-        presentation.enter_auth_code(auth_code, timeout=60.0)
-        if presentation.new_presentation is None:
-            msg = "Could not transit to `home`."
-            raise RuntimeError(msg)
-        presentation = presentation.new_presentation
+            presentation.enter_auth_code(auth_code, timeout=60.0)
+            if presentation.new_presentation is None:
+                msg = "Could not transit to `home`."
+                raise RuntimeError(msg)
+            presentation = presentation.new_presentation
 
         if not isinstance(presentation, HomePresentation):
             msg = "Could not transit to `home`."
