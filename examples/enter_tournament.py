a309d2a613c7c74b9e0e03436ead4942ba512b10
Apricot-S <mahjong_apricot@outlook.jp>
2024-02-16 08:08:38 +0900

    docs: add match part

--- examples/enter_tournament.py
+++ examples/enter_tournament.py
@@ -1,6 +1,7 @@
 import datetime
 import time
 from logging import INFO, StreamHandler, basicConfig
+from random import randint
 
 from majsoulrpa import RPA, config
 from majsoulrpa.presentation import (
@@ -8,6 +9,11 @@ from majsoulrpa.presentation import (
     HomePresentation,
     LoginPresentation,
     TournamentPresentation,
+)
+from majsoulrpa.presentation.match import MatchPresentation
+from majsoulrpa.presentation.match.operation import (
+    BabeiOperation,
+    DapaiOperation,
 )
 from majsoulrpa.yostar_login import YostarLoginIMAP
 
@@ -63,7 +69,75 @@ if __name__ == "__main__":
             raise RuntimeError(msg)
 
         if input("Do you want to prepare for match? y/n: ") == "y":
-            presentation.prepare(2)
+            presentation.prepare(90)
+
+            if presentation.new_presentation is None:
+                print("Matching did not start.")
+            else:
+                presentation = presentation.new_presentation
+
+                if not isinstance(presentation, MatchPresentation):
+                    msg = "Could not transit to `match`."
+                    raise RuntimeError(msg)
+
+                seat = None
+                for i, player in enumerate(presentation.players):
+                    if player.account_id == rpa.get_account_id():
+                        assert seat is None
+                        seat = i
+                    print(
+                        f"{player.name} ({player.level3}, {player.character})",
+                    )
+                assert seat is not None
+                changs = ["東", "南", "西", "北"]
+                print(
+                    f"{changs[presentation.chang]}{presentation.ju + 1}局"
+                    f"{presentation.ben}本場 (供託{presentation.liqibang}本)",
+                )
+                print(
+                    "スコア: "
+                    f"{','.join([str(s) for s in presentation.scores])}",
+                )
+                print(f"表ドラ表示牌: {presentation.dora_indicators[0]}")
+                print(f"自風: {changs[seat]}")
+                print(f"手牌: {','.join(presentation.shoupai)}")
+                if presentation.zimopai is not None:
+                    print(f"自摸牌: {presentation.zimopai}")
+
+                while True:
+                    ops = presentation.operation_list
+                    if ops is not None:
+                        op = None
+                        babei = False
+                        for op in ops:
+                            if isinstance(op, BabeiOperation):
+                                babei = True
+                                break
+                        if babei:
+                            time.sleep(0.1)
+                            presentation.select_operation(op)
+                            continue
+
+                        moqie = False
+                        for op in ops:
+                            if isinstance(op, DapaiOperation):
+                                moqie = True
+                                break
+                        if moqie:
+                            time.sleep(0.1)
+                            presentation.select_operation(op, randint(0, 13))  # noqa: S311
+                        else:
+                            presentation.select_operation(None)
+                    else:
+                        presentation.wait()
+
+                    if presentation.new_presentation is not None:
+                        presentation = presentation.new_presentation
+                        if isinstance(presentation, MatchPresentation):
+                            continue
+                        assert isinstance(presentation, TournamentPresentation)
+                        break
+
         presentation.leave()
 
         if presentation.new_presentation is None:
