130d83f235566781970399afa54e01b03a654539
smly <smly@users.noreply.github.com>
2024-02-28 15:37:31 +0900

    make liqi validation more strict

--- tests/test_presentation/test_match.py
+++ tests/test_presentation/test_match.py
@@ -42,7 +42,7 @@ def test_operate_liqi() -> None:
             "9s",
             "9s",
         ]
-        presentation._round_state._zimopai = "1m"
+        presentation._round_state.zimopai = "1m"  # type: ignore  # noqa: PGH003
 
         try:
             presentation._operate_liqi(op, 8)
@@ -56,3 +56,48 @@ def test_operate_liqi() -> None:
 
         with pytest.raises(InvalidOperationError):
             presentation._operate_liqi(op, 7)
+
+        # Test to discard zimopai. 05s are only allowed to be discarded.
+        with pytest.raises(InvalidOperationError):
+            presentation._operate_liqi(op, 13)
+
+        # Case2: Same hand but different zimopai.
+        op = LiqiOperation(combinations=["0s"])
+        assert op.candidate_dapai_list == ["0s"]
+
+        presentation = DummyMatchPresentation()
+        presentation._round_state.shoupai = [  # type: ignore  # noqa: PGH003
+            "1m",
+            "2m",
+            "7m",
+            "8m",
+            "9m",
+            "3p",
+            "4p",
+            "5p",
+            "4s",
+            "5s",  # <- index=9
+            "6s",
+            "9s",
+            "9s",
+        ]
+        presentation._round_state.zimopai = "0s"  # type: ignore  # noqa: PGH003
+
+        with pytest.raises(InvalidOperationError):
+            presentation._operate_liqi(op, 8)
+
+        try:
+            presentation._operate_liqi(op, 9)
+        except InvalidOperationError:
+            pytest.fail("InvalidOperationError raised unexpectedly")
+
+        # Test to discard zimopai. 05s are only allowed to be discarded.
+        try:
+            presentation._operate_liqi(op, 13)
+        except InvalidOperationError:
+            pytest.fail("InvalidOperationError raised unexpectedly")
+
+        # Test Liqi operation without zimopai.
+        presentation._round_state.zimopai = None  # type: ignore  # noqa: PGH003
+        with pytest.raises(InvalidOperationError):
+            presentation._operate_liqi(op, 9)
