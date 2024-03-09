7934a4f3a4d7ffc0d78cadc3aadba0071644c891
Apricot-S <mahjong_apricot@outlook.jp>
2024-03-09 18:05:22 +0900

    refactor: Simplify the module name

--- src/majsoulrpa/common/__init__.py
+++ src/majsoulrpa/common/__init__.py
@@ -1,5 +1,5 @@
 from .player import Player
-from .timeout_type import TimeoutType, timeout_to_deadline, to_timedelta
+from .timeout import TimeoutType, timeout_to_deadline, to_timedelta
 from .validation import validate_user_port
 
 __all__ = [
