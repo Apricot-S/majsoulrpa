f0595a5600f2346d73cf6aa7c421853cb1ca06ef
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-31 06:19:30 +0900

    feat: Remove code that is to be redesigned

--- src/majsoulrpa/__init__.py
+++ src/majsoulrpa/__init__.py
@@ -2,12 +2,10 @@
 
 from logging import NullHandler, getLogger
 
-from ._rpa import RPA
 from ._version import __version__
 
 getLogger(__name__).addHandler(NullHandler())
 
 __all__ = [
     "__version__",
-    "RPA",
 ]
