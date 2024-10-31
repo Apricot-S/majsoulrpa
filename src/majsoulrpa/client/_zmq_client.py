87b7817a2f9df73fc6b357a1d662c9e88e2930d1
Apricot-S <mahjong_apricot@outlook.jp>
2024-10-31 21:16:05 +0900

    refactor: Reorganize modules

---
+++ src/majsoulrpa/client/_zmq_client.py
@@ -0,0 +1,181 @@
+import base64
+import datetime
+import json
+from ipaddress import ip_address
+from pathlib import Path
+from typing import Any
+
+import google.protobuf.json_format
+import zmq
+from zmq.utils.win32 import allow_interrupt
+
+from majsoulrpa._impl.protocol import liqi_pb2
+from majsoulrpa.common import TimeoutType, to_timedelta
+
+from ._message_queue_client import Message, MessageQueueClientBase
+from ._validation import validate_user_port
+
+
+class ZMQClient(MessageQueueClientBase):
+    def __init__(self, host: str = "127.0.0.1", port: int = 37247) -> None:
+        ip_address(host)
+        validate_user_port(port)
+        super().__init__(host, port)
+        self._context = zmq.Context()
+        self._socket = self._context.socket(zmq.SUB)
+        self._socket.connect(f"tcp://{host}:{port}")
+        self._socket.subscribe(b"ws")
+        self._poller_in = zmq.Poller()
+        self._poller_in.register(self._socket, zmq.POLLIN)
+
+    def __del__(self) -> None:
+        self._poller_in.unregister(self._socket)
+        self._socket.close()
+        self._context.destroy()
+
+    def dequeue_message(self, timeout: TimeoutType) -> Message | None:
+        timeout = to_timedelta(timeout)
+
+        if timeout.total_seconds() <= 0.0:
+            return None
+
+        if len(self._put_back_messages) > 0:
+            return self._put_back_messages.popleft()
+
+        with allow_interrupt(self.__del__):
+            if self._poller_in.poll(int(timeout.total_seconds() * 1000)):
+                [_, message_bytes] = self._socket.recv_multipart()
+            else:
+                return None
+
+        message_str = message_bytes.decode(encoding="utf-8")
+        message = json.loads(message_str)
+        request_direction: str = message["request_direction"]
+        encoded_request: str = message["request"]
+        encoded_response: str | None = message["response"]
+        timestamp_float: float = message["timestamp"]
+
+        # Decode the data that was encoded for JSON.
+        request = base64.b64decode(encoded_request)
+        if encoded_response is not None:
+            response = base64.b64decode(encoded_response)
+        else:
+            response = None
+        timestamp = datetime.datetime.fromtimestamp(
+            timestamp_float,
+            datetime.UTC,
+        )
+
+        def unwrap_message(message: bytes) -> tuple[str, bytes]:
+            wrapper = liqi_pb2.Wrapper()  # type: ignore[attr-defined]
+            wrapper.ParseFromString(message)
+            return (wrapper.name, wrapper.data)
+
+        match request[0]:
+            # A request message that does not require a response
+            # is missing the two bytes of the message number.
+            case 1:
+                name, request_data = unwrap_message(request[1:])
+            # A request message that has a corresponding
+            # response message, there are 2 bytes to store
+            # the message number, and the name must be extracted to
+            # parse the response message.
+            case 2:
+                name, request_data = unwrap_message(request[3:])
+            case _:
+                msg = f"{request[0]}: unknown request type."
+                raise RuntimeError(msg)
+
+        if response is not None:
+            if response[0] != 3:  # noqa: PLR2004
+                msg = f"{response[0]}: unknown response type."
+                raise RuntimeError(msg)
+            response_name, response_data = unwrap_message(response[3:])
+            if response_name != "":
+                msg = f"{response_name}: unknown response name."
+                raise RuntimeError(msg)
+        else:
+            response_data = b""
+
+        # Convert Protocol Buffers messages to JSONizable object format
+        def jsonize(
+            name: str,
+            data: bytes,
+            *,
+            is_response: bool,
+        ) -> dict[str, Any]:
+            if is_response:
+                try:
+                    parser = self._message_type_map[name][1]()
+                except IndexError as ie:
+                    now = datetime.datetime.now(datetime.UTC)
+                    file_name = now.strftime(f"%Y-%m-%d-%H-%M-%S-{name}.bin")
+                    with Path(file_name).open("wb") as fp:
+                        fp.write(data)
+                    msg = (
+                        "A new API found:\n"
+                        f"  name: {name}\n"
+                        f"Raw data was saved to {file_name}.\n"
+                        "Please cooperate by providing data. "
+                        "Thank you for your cooperation."
+                    )
+                    raise RuntimeError(msg) from ie
+            else:
+                try:
+                    parser = self._message_type_map[name][0]()
+                except KeyError as ke:
+                    now = datetime.datetime.now(datetime.UTC)
+                    file_name = now.strftime(f"%Y-%m-%d-%H-%M-%S-{name}.bin")
+                    with Path(file_name).open("wb") as fp:
+                        fp.write(data)
+                    msg = (
+                        "A new API found:\n"
+                        f"  name: {name}\n"
+                        f"Raw data was saved to {file_name}.\n"
+                        "Please cooperate by providing data. "
+                        "Thank you for your cooperation."
+                    )
+                    raise RuntimeError(msg) from ke
+
+            parser.ParseFromString(data)
+
+            return google.protobuf.json_format.MessageToDict(
+                parser,
+                always_print_fields_with_no_presence=True,
+                preserving_proto_field_name=True,
+            )
+
+        jsonized_request = jsonize(name, request_data, is_response=False)
+        if response is not None:
+            jsonized_response = jsonize(name, response_data, is_response=True)
+        else:
+            jsonized_response = None
+
+        # If the message contains an account ID, extract the account ID.
+        if name in self._ACCOUNT_ID_MESSAGES:
+            if jsonized_response is None:
+                msg = "Message without any response."
+                raise RuntimeError(msg)
+            account_id = jsonized_response
+            keys = self._ACCOUNT_ID_MESSAGES[name]
+            for key in keys:
+                if key not in account_id:
+                    msg = (
+                        f"{name}: {key}: Could not find account id field:\n"
+                        f"{jsonized_response}"
+                    )
+                    raise RuntimeError(msg)
+                account_id = account_id[key]
+            if self._account_id is None:
+                self._account_id = account_id  # type: ignore[assignment]
+            elif account_id != self._account_id:
+                msg = "Inconsistent account IDs."
+                raise RuntimeError(msg)
+
+        return (
+            request_direction,
+            name,
+            jsonized_request,
+            jsonized_response,
+            timestamp,
+        )
---
+++ src/majsoulrpa/presentation/_template.py
@@ -0,0 +1,336 @@
+import datetime
+import tomllib
+from collections.abc import Iterable
+from pathlib import Path
+from typing import Final, Self
+
+import cv2
+import numpy as np
+
+from majsoulrpa.client._browser import STD_HEIGHT, STD_WIDTH, BrowserBase
+from majsoulrpa.common import TimeoutType, timeout_to_deadline
+
+from .exceptions import PresentationTimeoutError
+
+MIN_ZOOM_RATIO: Final[float] = 2.0 / 3
+MAX_ZOOM_RATIO: Final[float] = 2.0
+
+_STD_EDGE_SIGMA: Final[float] = 0.2
+_STD_THRESHOLD: Final[float] = 0.95
+
+_PATH_TEMPLATE: Final[Path] = Path(__file__).parents[1]
+
+
+def screenshot_to_opencv(screenshot_bytes: bytes) -> np.ndarray:
+    img_array = np.frombuffer(screenshot_bytes, np.uint8)
+    return cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+
+
+class Template:
+    def __init__(
+        self,
+        path: Path,
+        zoom_ratio: float,
+        *,
+        left: int = 0,
+        top: int = 0,
+        width: int = STD_WIDTH,
+        height: int = STD_HEIGHT,
+        threshold: float = _STD_THRESHOLD,
+    ) -> None:
+        if zoom_ratio < MIN_ZOOM_RATIO or zoom_ratio > MAX_ZOOM_RATIO:
+            msg = (
+                "Zoom ratio is only supported "
+                f"from {MIN_ZOOM_RATIO}x to {MAX_ZOOM_RATIO}x."
+            )
+            raise ValueError(msg)
+
+        self._path = path
+        self._zoom_ratio = zoom_ratio
+        self._left = int(left * zoom_ratio)
+        self._right = int((left + width) * zoom_ratio)
+        self._top = int(top * zoom_ratio)
+        self._bottom = int((top + height) * zoom_ratio)
+        self._width = int(width * zoom_ratio)
+        self._height = int(height * zoom_ratio)
+        self._threshold = threshold
+
+        templ = cv2.imread(str(path), cv2.IMREAD_COLOR)
+        if templ is None:
+            msg = "Failed to open template image file."
+            raise RuntimeError(msg)
+        if templ.shape[0] == 0:
+            msg = "The height of the template is equal to 0."
+            raise ValueError(msg)
+        if templ.shape[1] == 0:
+            msg = "The width of the template is equal to 0."
+            raise ValueError(msg)
+
+        self._templ = cv2.resize(templ, None, fx=zoom_ratio, fy=zoom_ratio)
+
+    @property
+    def img_width(self) -> int:
+        return self._templ.shape[1]
+
+    @property
+    def img_height(self) -> int:
+        return self._templ.shape[0]
+
+    @property
+    def threshold(self) -> float:
+        return self._threshold
+
+    @classmethod
+    def open_file(cls, name_or_path: str | Path, zoom_ratio: float) -> Self:
+        if isinstance(name_or_path, str):
+            path = _PATH_TEMPLATE / Path(name_or_path)
+        else:
+            path = _PATH_TEMPLATE / name_or_path
+
+        if path.suffix == ".toml":
+            pass
+        elif path.suffix == ".png":
+            if path.exists():
+                return cls(path, zoom_ratio)
+        elif path.suffix == "":
+            if (p := path.with_suffix(".toml")).exists():
+                path = p
+            elif (p := path.with_suffix(".png")).exists():
+                return cls(p, zoom_ratio)
+            else:
+                msg = f"{name_or_path}: an invalid template."
+                raise ValueError(msg)
+        else:
+            msg = f"{name_or_path}: an invalid template."
+            raise ValueError(msg)
+
+        if not path.exists():
+            msg = f"{path}: does not exist."
+            raise FileNotFoundError(msg)
+
+        with path.open("rb") as f:
+            config = tomllib.load(f)
+
+        if "path" in config:
+            png_path_str: str = config["path"]
+            if png_path_str.startswith("./"):
+                png_path = path.parent / png_path_str
+            else:
+                png_path = Path(png_path_str)
+            del config["path"]
+        else:
+            png_path = path.with_suffix(".png")
+
+        if not png_path.exists():
+            msg = f"{path}: does not exist."
+            raise FileNotFoundError(msg)
+
+        return cls(png_path, zoom_ratio, **config)
+
+    def best_template_match(self, screenshot: bytes) -> tuple[int, int, float]:
+        image = screenshot_to_opencv(screenshot)
+        image = image[self._top : self._bottom, self._left : self._right, :]
+
+        if image.shape[0] < self._templ.shape[0]:
+            msg = (
+                f"The height of the screenshot ({image.shape[0]}) is smaller"
+                f"than the template's ({self._templ.shape[0]})."
+            )
+            raise ValueError(msg)
+        if image.shape[1] < self._templ.shape[1]:
+            msg = (
+                f"The width of the screenshot ({image.shape[1]}) is smaller"
+                f" than the template's ({self._templ.shape[1]})."
+            )
+            raise ValueError(msg)
+
+        result1 = cv2.matchTemplate(image, self._templ, cv2.TM_CCOEFF_NORMED)
+        result2 = cv2.matchTemplate(image, self._templ, cv2.TM_SQDIFF_NORMED)
+        _, max_val1, _, max_loc1 = cv2.minMaxLoc(result1)
+        min_val2, _, min_loc2, _ = cv2.minMaxLoc(result2)
+
+        if max_val1 >= (1.0 - min_val2):
+            argmax_x, argmax_y = max_loc1
+            max_score = max_val1
+        else:
+            argmax_x, argmax_y = min_loc2
+            max_score = 1.0 - min_val2
+
+        return (self._left + argmax_x, self._top + argmax_y, max_score)
+
+    def match(self, screenshot: bytes) -> bool:
+        _, _, score = self.best_template_match(screenshot)
+        return score >= self._threshold
+
+    def wait_until(
+        self,
+        browser: BrowserBase,
+        deadline: datetime.datetime,
+    ) -> None:
+        while True:
+            if datetime.datetime.now(datetime.UTC) > deadline:
+                msg = f"Timeout in waiting {self._path}"
+                raise PresentationTimeoutError(msg, browser.get_screenshot())
+            if self.match(browser.get_screenshot()):
+                break
+
+    def wait_for(self, browser: BrowserBase, timeout: TimeoutType) -> None:
+        deadline = timeout_to_deadline(timeout)
+        self.wait_until(browser, deadline)
+
+    def click(
+        self,
+        browser: BrowserBase,
+        edge_sigma: float = _STD_EDGE_SIGMA,
+    ) -> None:
+        x, y, _ = self.best_template_match(browser.get_screenshot())
+        browser.click_region(
+            x,
+            y,
+            self._templ.shape[1],
+            self._templ.shape[0],
+            edge_sigma,
+        )
+
+    def click_if_match(
+        self,
+        browser: BrowserBase,
+        edge_sigma: float = _STD_EDGE_SIGMA,
+    ) -> bool:
+        x, y, score = self.best_template_match(browser.get_screenshot())
+        if score >= self._threshold:
+            browser.click_region(
+                x,
+                y,
+                self._templ.shape[1],
+                self._templ.shape[0],
+                edge_sigma,
+            )
+            return True
+        return False
+
+    def wait_until_then_click(
+        self,
+        browser: BrowserBase,
+        deadline: datetime.datetime,
+        edge_sigma: float = _STD_EDGE_SIGMA,
+    ) -> None:
+        while True:
+            if datetime.datetime.now(datetime.UTC) > deadline:
+                msg = f"Timeout in waiting {self._path}"
+                raise PresentationTimeoutError(msg, browser.get_screenshot())
+            x, y, score = self.best_template_match(browser.get_screenshot())
+            if score >= self._threshold:
+                break
+
+        browser.click_region(
+            x,
+            y,
+            self._templ.shape[1],
+            self._templ.shape[0],
+            edge_sigma,
+        )
+
+    def wait_for_then_click(
+        self,
+        browser: BrowserBase,
+        timeout: TimeoutType,
+        edge_sigma: float = _STD_EDGE_SIGMA,
+    ) -> None:
+        deadline = timeout_to_deadline(timeout)
+        self.wait_until_then_click(browser, deadline, edge_sigma)
+
+    @staticmethod
+    def match_one_of(
+        screenshot: bytes,
+        templates: Iterable["Template"],
+    ) -> int:
+        for i, template in enumerate(templates):
+            if template.match(screenshot):
+                return i
+        return -1
+
+    @staticmethod
+    def wait_until_one_of(
+        templates: Iterable["Template"],
+        browser: BrowserBase,
+        deadline: datetime.datetime,
+    ) -> None:
+        while True:
+            if datetime.datetime.now(datetime.UTC) > deadline:
+                msg = "Timeout"
+                raise PresentationTimeoutError(msg, browser.get_screenshot())
+
+            screenshot = browser.get_screenshot()
+            for template in templates:
+                if template.match(screenshot):
+                    return
+
+    @staticmethod
+    def wait_for_one_of(
+        templates: Iterable["Template"],
+        browser: BrowserBase,
+        timeout: TimeoutType,
+    ) -> None:
+        deadline = timeout_to_deadline(timeout)
+        Template.wait_until_one_of(templates, browser, deadline)
+
+    @staticmethod
+    def click_if_match_one_of(
+        templates: Iterable["Template"],
+        browser: BrowserBase,
+        edge_sigma: float = _STD_EDGE_SIGMA,
+    ) -> int:
+        screenshot = browser.get_screenshot()
+        for i, template in enumerate(templates):
+            x, y, score = template.best_template_match(screenshot)
+            if score >= template.threshold:
+                browser.click_region(
+                    x,
+                    y,
+                    template.img_width,
+                    template.img_height,
+                    edge_sigma,
+                )
+                return i
+        return -1
+
+    @staticmethod
+    def wait_until_one_of_then_click(
+        templates: Iterable["Template"],
+        browser: BrowserBase,
+        deadline: datetime.datetime,
+        edge_sigma: float = _STD_EDGE_SIGMA,
+    ) -> None:
+        while True:
+            if datetime.datetime.now(datetime.UTC) > deadline:
+                msg = "Timeout"
+                raise PresentationTimeoutError(msg, browser.get_screenshot())
+
+            screenshot = browser.get_screenshot()
+            for template in templates:
+                x, y, score = template.best_template_match(screenshot)
+                if score >= template.threshold:
+                    browser.click_region(
+                        x,
+                        y,
+                        template.img_width,
+                        template.img_height,
+                        edge_sigma,
+                    )
+                    return
+
+    @staticmethod
+    def wait_for_one_of_then_click(
+        templates: Iterable["Template"],
+        browser: BrowserBase,
+        timeout: TimeoutType,
+        edge_sigma: float = _STD_EDGE_SIGMA,
+    ) -> None:
+        deadline = timeout_to_deadline(timeout)
+        Template.wait_until_one_of_then_click(
+            templates,
+            browser,
+            deadline,
+            edge_sigma,
+        )
