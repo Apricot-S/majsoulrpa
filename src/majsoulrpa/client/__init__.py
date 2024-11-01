19a736302db6a9503cef2e89f4fd590f6eb290f7
Apricot-S <mahjong_apricot@outlook.jp>
2024-11-02 04:29:14 +0900

    docs: Add docstring

---
+++ src/majsoulrpa/client/__init__.py
@@ -0,0 +1,1 @@
+"""Provides a RPA client."""
--- src/majsoulrpa/common/player.py
+++ src/majsoulrpa/common/player.py
@@ -1,12 +1,22 @@
 class Player:
+    """Player."""
+
     def __init__(self, account_id: int, name: str) -> None:
+        """Initializes the instance.
+
+        Args:
+            account_id: Player's account ID.
+            name: Player's name.
+        """
         self._account_id = account_id
         self._name = name
 
     @property
     def account_id(self) -> int:
+        """Player's account ID."""
         return self._account_id
 
     @property
     def name(self) -> str:
+        """Player's name."""
         return self._name
