192adf8c4e863f03fa9625c90fa71ca783cb7eeb
Apricot-S <mahjong_apricot@outlook.jp>
2024-11-01 10:25:24 +0900

    test: Add test cases

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,3 +1,4 @@
+import io
 from pathlib import Path
 
 from pytest_mock import MockerFixture
@@ -304,6 +305,91 @@ def test_get_config_single(mocker: MockerFixture) -> None:
             },
             "viewport_height": 720,
             "headless": False,
+            "user_data_dir": "./user_data",
+        },
+    }
+
+    actual = get_config(config_path)
+    assert actual == expected
+
+
+def test_get_config_multiple(mocker: MockerFixture) -> None:
+    config_path = Path("./config.toml")
+    config_data = b"""
+    [[majsoulrpa]]
+    remote_host = "127.0.0.1"
+    [majsoulrpa.authentication]
+    email_address = "your_email_address@example.cfg"
+    [majsoulrpa.port]
+    remote_port = 19222
+    proxy_port = 8080
+    message_queue_port = 37247
+    [majsoulrpa.browser]
+    initial_position = { left = 100, top = 100 }
+    viewport_height = 720
+    headless = false
+    user_data_dir = "./user_data"
+
+    [[majsoulrpa]]
+    name = "Desktop headless IMAP 1920 x 1080"
+    [majsoulrpa.authentication]
+    email_address = "your_email_address_two@example.cfg"
+    imap_server = "imap.server.cfg"
+    password = "app_password"
+    mail_folder = "INBOX"
+    [majsoulrpa.browser]
+    initial_position = { left = -2150, top = -360 }
+    headless = true
+    user_data_dir = "./user_data"
+    """
+
+    mocker.patch(
+        "pathlib.Path.open",
+        new_callable=mocker.mock_open,
+        read_data=config_data,
+    )
+    mocker.patch("sys.stdin", io.StringIO("0\n"))
+
+    expected = {
+        "remote_host": "127.0.0.1",
+        "authentication": {
+            "email_address": "your_email_address@example.cfg",
+        },
+        "port": {
+            "remote_port": 19222,
+            "proxy_port": 8080,
+            "message_queue_port": 37247,
+        },
+        "browser": {
+            "initial_position": {
+                "left": 100,
+                "top": 100,
+            },
+            "viewport_height": 720,
+            "headless": False,
+            "user_data_dir": "./user_data",
+        },
+    }
+
+    actual = get_config(config_path)
+    assert actual == expected
+
+    mocker.patch("sys.stdin", io.StringIO("1\n"))
+
+    expected = {
+        "name": "Desktop headless IMAP 1920 x 1080",
+        "authentication": {
+            "email_address": "your_email_address_two@example.cfg",
+            "imap_server": "imap.server.cfg",
+            "password": "app_password",
+            "mail_folder": "INBOX",
+        },
+        "browser": {
+            "initial_position": {
+                "left": -2150,
+                "top": -360,
+            },
+            "headless": True,
             "user_data_dir": "./user_data",
         },
     }
