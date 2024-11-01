d44b54765e5566a2052a8e5790376e024bb28ec6
Apricot-S <mahjong_apricot@outlook.jp>
2024-11-02 05:05:53 +0900

    docs: Add docstring

---
+++ src/majsoulrpa/presentation/__init__.py
@@ -0,0 +1,8 @@
+"""Provides presentation classes.
+
+In this context, a *presentation* refers to a specific screen state of
+the game. Each presentation acts as both the View and Controller in the
+MVC (Model-View-Controller) pattern. As the View, it is tasked with
+detecting and recognizing the screen state. As the Controller, it
+manages interactions with widgets on the screen.
+"""
