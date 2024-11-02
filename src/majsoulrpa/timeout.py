129dd580da554320801b013285a600e766309c76
Apricot-S <mahjong_apricot@outlook.jp>
2024-11-02 19:48:07 +0900

    docs: Add docstrings

--- src/majsoulrpa/timeout.py
+++ src/majsoulrpa/timeout.py
@@ -1,9 +1,24 @@
+"""Provides functionality for handling timeout duration."""
+
 import datetime
 
 type TimeoutType = int | float | datetime.timedelta
+"""A type representing timeout duration.
+
+    The unit is seconds.
+"""
 
 
 def to_timedelta(seconds: TimeoutType) -> datetime.timedelta:
+    """Converts `TimeoutType` to `datetime.timedelta`.
+
+    Args:
+        seconds: Timeout duration in seconds.
+
+    Returns:
+        A `datetime.timedelta` object that represents the same timeout
+            duration as the input `seconds`.
+    """
     if isinstance(seconds, datetime.timedelta):
         return seconds
     if isinstance(seconds, int | float):
@@ -14,4 +29,12 @@ def to_timedelta(seconds: TimeoutType) -> datetime.timedelta:
 
 
 def timeout_to_deadline(timeout: TimeoutType) -> datetime.datetime:
+    """Converts timeout duration to a deadline from the current time.
+
+    Args:
+        timeout: Timeout duration in seconds.
+
+    Returns:
+        A deadline from the current time.
+    """
     return datetime.datetime.now(datetime.UTC) + to_timedelta(timeout)
