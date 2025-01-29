e4bf0c9b48d7ce384dd6a2812f7115ebc6b1be5a
Apricot-S <mahjong_apricot@outlook.jp>
2025-01-29 22:49:54 +0900

    feat: Make a addon file private

--- src/majsoulrpa/client/sniffer_addon/__init__.py
+++ src/majsoulrpa/client/sniffer_addon/__init__.py
@@ -6,4 +6,4 @@ Sends messages sniffed by mitmproxy to a message queue.
 from pathlib import Path
 from typing import Final
 
-SNIFFER_ADDON_PATH_ZMQ: Final = Path(__file__).parent / "zmq.py"
+SNIFFER_ADDON_PATH: Final = Path(__file__).parent / "_zmq.py"
--- src/majsoulrpa/client/sniffer_addon/_zmq.py
+++ src/majsoulrpa/client/sniffer_addon/_zmq.py
@@ -1,4 +1,4 @@
-# ruff: noqa: D100, D101, D102, D107, S101
+# ruff: noqa: S101
 
 import base64
 import datetime
