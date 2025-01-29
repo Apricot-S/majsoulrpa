1567303d5d3616c6a185916c0da027a833328fd0
Apricot-S <mahjong_apricot@outlook.jp>
2025-01-29 22:31:50 +0900

    feat: browser (WIP)

--- src/majsoulrpa/client/browser/__init__.py
+++ src/majsoulrpa/client/browser/__init__.py
@@ -3,3 +3,13 @@
 from .base import STD_HEIGHT, STD_WIDTH, BrowserBase
 from .local import LocalBrowser, LocalBrowserBase
 from .remote import RemoteBrowser, RemoteBrowserBase
+
+__all__ = [
+    "STD_HEIGHT",
+    "STD_WIDTH",
+    "BrowserBase",
+    "LocalBrowser",
+    "LocalBrowserBase",
+    "RemoteBrowser",
+    "RemoteBrowserBase",
+]
