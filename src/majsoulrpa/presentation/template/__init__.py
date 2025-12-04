d44f8cca0dc3905e44ffdba7048543fdc4280ab1
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-05 01:36:57 +0900

    feat: Develop (WIP)

--- src/majsoulrpa/presentation/template/__init__.py
+++ src/majsoulrpa/presentation/template/__init__.py
@@ -1,7 +1,8 @@
 from majsoulrpa.presentation.template.config import Config
-from majsoulrpa.presentation.template.image import FileImage
+from majsoulrpa.presentation.template.image import IMAGE_PATH, FileImage
 
 __all__ = [
+    "IMAGE_PATH",
     "Config",
     "FileImage",
 ]
