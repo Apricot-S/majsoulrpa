a93aab84d1575e7649d7ace2337f10e637584603
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-06 01:30:26 +0900

    docs: Update an example

--- examples/login.py
+++ examples/login.py
@@ -6,26 +6,25 @@ from typing import Any
 from majsoulrpa import RPAClient
 from majsoulrpa.presentation.login import LoginPresentation
 
+rpa = RPAClient()
 
-async def callback_login(
+
+@rpa.on(LoginPresentation)
+async def on_login(
     p: LoginPresentation,
     data: Any,
 ) -> tuple[LoginPresentation, Any]:
     async with asyncio.timeout(30):
         await p.enter_email_address("majsoul-rpa-dev@example.com")
-        await asyncio.sleep(3)
+        await asyncio.sleep(2)
         await p.end(close_browser=True)
 
     return p, data + 1
 
 
 async def main() -> None:
-    rpa = RPAClient()
-    rpa.on(LoginPresentation)(callback_login)
-
     config = RPAClient.Config("127.0.0.1", 19222)
     await rpa.run(config, data=0, detection_timeout=30)
-
     print("Done!!!")
 
 
