bc146cac2bd3d139e7c0f50e7ad67b10d1e76e21
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-06 16:13:36 +0900

    refactor: Add default runners

--- src/majsoulrpa/browser/server/runtime.py
+++ src/majsoulrpa/browser/server/runtime.py
@@ -5,13 +5,19 @@ from majsoulrpa.browser.server.config import Config
 from majsoulrpa.sniffer import ADDON_PATH
 
 
-def run_browser_server_impl(
+def run_browser_server(
     config: Config,
     option: engine.Option,
-    server_runner: core.ServerRunner,
-    browser_engine_runner: engine.BrowserEngineRunner,
-    sniffer_runner: sniffer.SnifferRunner,
+    server_runner: core.ServerRunner | None = None,
+    browser_engine_runner: engine.BrowserEngineRunner | None = None,
+    sniffer_runner: sniffer.SnifferRunner | None = None,
 ) -> None:
+    server_runner = server_runner or core.run_server
+    browser_engine_runner = browser_engine_runner or engine.run_browser_engine
+    sniffer_runner = sniffer_runner or (
+        lambda c: sniffer.run_sniffer(c, ADDON_PATH)
+    )
+
     sniffer_process = sniffer_runner(config)
 
     try:
@@ -19,13 +25,3 @@ def run_browser_server_impl(
     finally:
         if sniffer_process.poll() is None:
             sniffer_process.terminate()
-
-
-def run_browser_server(config: Config, option: engine.Option) -> None:
-    run_browser_server_impl(
-        config,
-        option,
-        core.run_server,
-        engine.run_browser_engine,
-        lambda c: sniffer.run_sniffer(c, ADDON_PATH),
-    )
