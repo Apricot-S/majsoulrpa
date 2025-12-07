b6df7cd2985ac927626ae7f700649e8d2d218058
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-08 00:51:17 +0900

    feat: Develop (WIP)

--- src/majsoulrpa/presentation/base.py
+++ src/majsoulrpa/presentation/base.py
@@ -1,5 +1,6 @@
 import asyncio
 from abc import ABC, abstractmethod
+from collections.abc import Iterable
 from typing import Self
 
 from majsoulrpa import browser
@@ -63,6 +64,17 @@ class Presentation(ABC):
         region = matcher.match(screen, self._scale)
         return region is not None
 
+    async def _has_match_one_of(
+        self,
+        matchers: Iterable[template.MatcherBase],
+    ) -> bool:
+        screen = await self.get_screenshot()
+        for matcher in matchers:
+            region = matcher.match(screen, self._scale)
+            if region is not None:
+                return True
+        return False
+
     async def _wait_until_match(
         self,
         matcher: template.MatcherBase,
@@ -70,6 +82,16 @@ class Presentation(ABC):
     ) -> None:
         while True:
             if await self._has_match(matcher):
+                return
+            await asyncio.sleep(interval)
+
+    async def _wait_until_match_one_of(
+        self,
+        matchers: Iterable[template.MatcherBase],
+        interval: float = 0.5,
+    ) -> None:
+        while True:
+            if await self._has_match_one_of(matchers):
                 return
             await asyncio.sleep(interval)
 
@@ -90,6 +112,23 @@ class Presentation(ABC):
         await self._driver.click_mouse(x, y, d)
         return True
 
+    async def _click_if_match_one_of(
+        self,
+        matchers: Iterable[template.MatcherBase],
+        edge_sigma: float = DEFAULT_EDGE_SIGMA,
+        base_delay: float = 100,
+        delay_sigma: float = 0.1,
+    ) -> bool:
+        screen = await self.get_screenshot()
+        for matcher in matchers:
+            region = matcher.match(screen, self._scale)
+            if region is not None:
+                x, y = get_random_point_in_region(region, edge_sigma)
+                d = get_random_delay(base_delay, delay_sigma)
+                await self._driver.click_mouse(x, y, d)
+                return True
+        return False
+
     async def _click_when_match(
         self,
         matcher: template.MatcherBase,
@@ -109,6 +148,25 @@ class Presentation(ABC):
             d = get_random_delay(base_delay, delay_sigma)
             await self._driver.click_mouse(x, y, d)
             return
+
+    async def _click_when_match_one_of(
+        self,
+        matchers: Iterable[template.MatcherBase],
+        edge_sigma: float = DEFAULT_EDGE_SIGMA,
+        base_delay: float = 100,
+        delay_sigma: float = 0.1,
+        interval: float = 0.5,
+    ) -> None:
+        while True:
+            screen = await self.get_screenshot()
+            for matcher in matchers:
+                region = matcher.match(screen, self._scale)
+                if region is not None:
+                    x, y = get_random_point_in_region(region, edge_sigma)
+                    d = get_random_delay(base_delay, delay_sigma)
+                    await self._driver.click_mouse(x, y, d)
+                    return
+            await asyncio.sleep(interval)
 
     @classmethod
     @abstractmethod
