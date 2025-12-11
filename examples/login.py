30b88f315f7ab19dd5ace63ffc94f12ce05b2637
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-12 02:11:13 +0900

    docs: Extend the wait time in the example

--- examples/login.py
+++ examples/login.py
@@ -13,11 +13,11 @@ rpa = RPAClient()
 @rpa.on(LoginPresentation)
 async def on_login(p: LoginPresentation, data: Any) -> Any:
     email_address = input("Enter your email address: ")
-    async with asyncio.timeout(10):
+    async with asyncio.timeout(30):
         await p.enter_email_address(email_address)
 
     verification_code = input("Enter the verification code: ")
-    async with asyncio.timeout(10):
+    async with asyncio.timeout(30):
         await p.enter_verification_code(verification_code)
 
     return [data, 1]
