51f42e1df151963d4e8c2546038cf59a5cd5af92
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-12 14:42:40 +0900

    docs: Add examples

---
+++ examples/fetch_log_browser.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from subprocess import Popen
+
+from majsoulrpa.browser.server.cli import CommandLineArgs
+from majsoulrpa.browser.server.config import Config
+from majsoulrpa.browser.server.runtime import run_browser_server
+from majsoulrpa.browser.server.sniffer import run_sniffer
+
+ADDON_PATH = Path(__file__).parent / "fetch_log_addon.py"
+
+
+def main() -> None:
+    def archiver_runner(config: Config) -> Popen:
+        return run_sniffer(config, ADDON_PATH)
+
+    args = CommandLineArgs(headless=True)
+    config = args.to_config()
+    option = args.to_option()
+    run_browser_server(config, option, sniffer_runner=archiver_runner)
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/fetch_log_client.py
@@ -0,0 +1,57 @@
+# ruff: noqa: ANN401
+
+import asyncio
+from typing import Any
+
+from majsoulrpa import RPAClient
+from majsoulrpa.presentation.base import require_active
+from majsoulrpa.presentation.home import HomePresentation
+from majsoulrpa.presentation.login import LoginPresentation
+
+
+class FetchLogPresentation(HomePresentation):
+    @require_active
+    async def fetch_log(self, log_id: str) -> None:
+        await self._driver.goto_log(log_id)
+        while True:
+            message = await self._message_queue.get()
+            if message.name == ".lq.Lobby.fetchGameRecord":
+                return
+
+
+rpa = RPAClient()
+
+
+@rpa.on(LoginPresentation)
+async def on_login(p: LoginPresentation, data: Any) -> Any:
+    email_address = input("Enter your email address: ")
+    async with asyncio.timeout(30):
+        await p.enter_email_address(email_address)
+
+    verification_code = input("Enter the verification code: ")
+    async with asyncio.timeout(30):
+        await p.enter_verification_code(verification_code)
+
+    return data
+
+
+@rpa.on(FetchLogPresentation)
+async def on_home(p: FetchLogPresentation, data: Any) -> Any:
+    log_id = input("Enter the log id: ")
+    async with asyncio.timeout(120):
+        await p.fetch_log(log_id)
+
+    async with asyncio.timeout(5):
+        await p.end_rpa(close_browser=True)
+
+    return data
+
+
+async def main() -> None:
+    config = RPAClient.Config()
+    await rpa.run(config, None, detection_timeout=30)
+    print("The RPA client has been terminated.")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
