791737b3a4c83583fac62a77b0339ede1a4a353a
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-15 03:33:52 +0900

    fix: Fix bug when serialize

--- src/majsoulrpa/config_input/config.py
+++ src/majsoulrpa/config_input/config.py
@@ -6,7 +6,10 @@ from majsoulrpa.config_input.endpoint import Endpoint
 
 
 class ConfigInput(BaseModel):
-    model_config = ConfigDict(alias_generator=_to_kebab)
+    model_config = ConfigDict(
+        alias_generator=_to_kebab,
+        serialize_by_alias=True,
+    )
 
     endpoint: Endpoint = Field(default_factory=Endpoint)
     browser: Browser = Field(default_factory=Browser)
