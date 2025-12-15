c8102330ba820c8d3ef87c46ba6e6a31165d6461
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-15 09:40:41 +0900

    test: Add test cases

--- tests/config_input/test_config.py
+++ tests/config_input/test_config.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 from majsoulrpa import constants
 from majsoulrpa.config_input.config import ConfigInput
 
@@ -19,5 +21,29 @@ def test_defaults_to_dict() -> None:
             "user-data-dir": None,
         },
     }
-    actual = ConfigInput().model_dump()
+    assert ConfigInput().model_dump() == expected
+
+
+def test_ignore_extra() -> None:
+    expected: dict = {
+        "endpoint": {
+            "browser-address": constants.DEFAULT_BROWSER_ADDRESS,
+            "client-address": constants.DEFAULT_CLIENT_ADDRESS,
+            "remote-port": constants.DEFAULT_REMOTE_PORT,
+            "sniffer-port": constants.DEFAULT_SNIFFER_PORT,
+            "proxy-port": constants.DEFAULT_PROXY_PORT,
+        },
+        "browser": {
+            "window-left": 0,
+            "window-top": 0,
+            "viewport-height": constants.DEFAULT_VIEWPORT_HEIGHT,
+            "headless": False,
+            "user-data-dir": None,
+        },
+    }
+
+    input_dict = deepcopy(expected)
+    input_dict["extra"] = {"extra1": 1}
+
+    actual = ConfigInput().model_validate(input_dict).model_dump()
     assert actual == expected
