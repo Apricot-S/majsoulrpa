07c257adb7ff3bfeb571ef95ea410e2b6c0fae82
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-15 15:21:39 +0900

    feat: Export classes

--- src/majsoulrpa/config_input/__init__.py
+++ src/majsoulrpa/config_input/__init__.py
@@ -1,5 +1,9 @@
+from majsoulrpa.config_input.browser import Browser
 from majsoulrpa.config_input.config import ConfigInput
+from majsoulrpa.config_input.endpoint import Endpoint
 
 __all__ = [
+    "Browser",
     "ConfigInput",
+    "Endpoint",
 ]
