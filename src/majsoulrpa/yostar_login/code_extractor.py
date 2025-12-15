ec2b10bdd4c9f67afe01bffbd5f0b92130f5c32b
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-16 00:13:39 +0900

    feat: Develop CodeExtractor (WIP)

--- src/majsoulrpa/yostar_login/code_extractor.py
+++ src/majsoulrpa/yostar_login/code_extractor.py
@@ -1,2 +1,15 @@
-class CodeExtractor:
-    pass
+from abc import ABC, abstractmethod
+from email.message import EmailMessage
+from typing import override
+
+
+class CodeExtractorBase(ABC):
+    @abstractmethod
+    def extract_code(self, email_message: EmailMessage) -> str | None:
+        pass
+
+
+class CodeExtractor(CodeExtractorBase):
+    @override
+    def extract_code(self, email_message: EmailMessage) -> str | None:
+        raise NotImplementedError
