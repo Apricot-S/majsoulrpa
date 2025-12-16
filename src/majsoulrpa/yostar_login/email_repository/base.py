c329a0e8e116692b00210a61a480b36cc8baa690
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-16 17:26:02 +0900

    feat: Develop EmailRepositoryBase

--- src/majsoulrpa/yostar_login/email_repository/base.py
+++ src/majsoulrpa/yostar_login/email_repository/base.py
@@ -1,5 +1,17 @@
-from abc import ABC
+from abc import ABC, abstractmethod
+from email.message import EmailMessage
+from typing import Self
 
 
 class EmailRepositoryBase(ABC):
-    pass
+    @abstractmethod
+    def __aiter__(self) -> Self:
+        pass
+
+    @abstractmethod
+    async def __anext__(self) -> tuple[str, EmailMessage]:
+        pass
+
+    @abstractmethod
+    async def delete_message(self, key: str) -> None:
+        pass
