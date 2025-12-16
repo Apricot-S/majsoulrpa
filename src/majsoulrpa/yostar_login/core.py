20a2c7f8318c60e7db5a97a515718c5eb88105e0
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-16 17:32:41 +0900

    feat: Develop YostarLogin (WIP)

--- src/majsoulrpa/yostar_login/core.py
+++ src/majsoulrpa/yostar_login/core.py
@@ -1,2 +1,27 @@
+from majsoulrpa.yostar_login.code_extractor import (
+    CodeExtractor,
+    CodeExtractorBase,
+)
+from majsoulrpa.yostar_login.email_classifier import (
+    EmailClassifier,
+    EmailClassifierBase,
+)
+from majsoulrpa.yostar_login.email_repository import EmailRepositoryBase
+
+
 class YostarLogin:
-    pass
+    def __init__(
+        self,
+        repository: EmailRepositoryBase,
+        classifier: EmailClassifierBase | None = None,
+        extractor: CodeExtractorBase | None = None,
+    ) -> None:
+        self._repository = repository
+        if classifier is None:
+            self._classifier: EmailClassifierBase = EmailClassifier()
+        else:
+            self._classifier = classifier
+        if extractor is None:
+            self._extractor: CodeExtractorBase = CodeExtractor()
+        else:
+            self._extractor = extractor
