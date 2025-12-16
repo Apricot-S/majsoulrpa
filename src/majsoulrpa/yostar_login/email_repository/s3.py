16c87a3b623808e08ead58bfd2f70700dd68ef8c
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-16 19:30:13 +0900

    feat: Develop S3EmailRepository (WIP)

--- src/majsoulrpa/yostar_login/email_repository/s3.py
+++ src/majsoulrpa/yostar_login/email_repository/s3.py
@@ -1,5 +1,31 @@
+from email.message import EmailMessage
+from typing import Self, override
+
+import boto3
+
 from majsoulrpa.yostar_login.email_repository.base import EmailRepositoryBase
 
 
 class S3EmailRepository(EmailRepositoryBase):
-    pass
+    def __init__(
+        self,
+        bucket_name: str,
+        key_prefix: str,
+        aws_profile: str | None,
+    ) -> None:
+        session = boto3.Session(profile_name=aws_profile)
+        s3_client = session.resource("s3")
+        self._s3_bucket = s3_client.Bucket(bucket_name)
+        self._key_prefix = key_prefix
+
+    @override
+    def __aiter__(self) -> Self:
+        raise NotImplementedError
+
+    @override
+    async def __anext__(self) -> tuple[str, EmailMessage]:
+        raise NotImplementedError
+
+    @override
+    async def delete_message(self, key: str) -> None:
+        raise NotImplementedError
