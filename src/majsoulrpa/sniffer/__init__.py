16e6558a8fb3616cd90c2a72c33e773ef22953dc
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-19 02:17:48 +0900

    feat: Change import

--- src/majsoulrpa/sniffer/__init__.py
+++ src/majsoulrpa/sniffer/__init__.py
@@ -1,8 +1,26 @@
 from pathlib import Path
+from typing import TYPE_CHECKING
 
-from majsoulrpa.sniffer.message_queue import MessageQueue, MessageQueueBase
+if TYPE_CHECKING:
+    from majsoulrpa.sniffer.message_queue import MessageQueue, MessageQueueBase
+else:
+    try:
+        from majsoulrpa.sniffer.message_queue import (
+            MessageQueue,
+            MessageQueueBase,
+        )
+    except ModuleNotFoundError:
+
+        class _MessageQueueUnavailable:
+            def __init__(self, *args, **kwargs) -> None:  # noqa: ARG002
+                msg = "`MessageQueue` requires `client` extra"
+                raise RuntimeError(msg)
+
+        MessageQueue = _MessageQueueUnavailable  # type: ignore[assignment,misc]
+        MessageQueueBase = _MessageQueueUnavailable  # type: ignore[assignment,misc]
 
 ADDON_PATH = Path(__file__).parent / "addon.py"
+
 
 __all__ = [
     "MessageQueue",
