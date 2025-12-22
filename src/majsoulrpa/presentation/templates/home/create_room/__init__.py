0f2bb2ebd686f6d477ed2cdd0b47faf083d0e3ee
Apricot-S <mahjong_apricot@outlook.jp>
2025-12-23 01:28:50 +0900

    feat: Develop create_room (WIP)

--- src/majsoulrpa/presentation/templates/home/create_room/__init__.py
+++ src/majsoulrpa/presentation/templates/home/create_room/__init__.py
@@ -8,6 +8,11 @@ _CREATE_IMAGE = FileImage(_PARENT / "create.png")
 _CREATE_CONFIG = Config.from_file(_PARENT / "create.toml")
 CREATE = Matcher(_CREATE_IMAGE, _CREATE_CONFIG)
 
+_UNSELECTED_IMAGE = FileImage(_PARENT / "unselected.png")
+
+_FOUR_PLAYER_CONFIG = Config.from_file(_PARENT / "4-player.toml")
+FOUR_PLAYER = Matcher(_UNSELECTED_IMAGE, _FOUR_PLAYER_CONFIG)
+
 __all__ = [
     "CREATE",
 ]
