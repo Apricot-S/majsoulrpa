406f63f28de04ffeeaa0de844673372dad6fdc9f
Apricot-S <mahjong_apricot@outlook.jp>
2026-03-15 06:02:50 +0900

    fix: Update regions for login

--- src/majsoulrpa/presentation/regions/login.py
+++ src/majsoulrpa/presentation/regions/login.py
@@ -1,5 +1,5 @@
 from majsoulrpa.presentation.region import Region
 
-EMAIL_ADDRESS_FIELD = Region(365, 385, 200, 30)
-SEND_CODE = Region(850, 500, 190, 70)
-VERIFICATION_CODE_FIELD = Region(365, 520, 140, 35)
+EMAIL_ADDRESS_FIELD = Region(770, 430, 138, 20)
+SEND_CODE = Region(1102, 508, 40, 22)
+VERIFICATION_CODE_FIELD = Region(768, 508, 100, 20)
