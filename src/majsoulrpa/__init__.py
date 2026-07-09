d0b4b711617f1a00f1b225e8657cad97bffcb753
Apricot-S <mahjong_apricot@outlook.jp>
2026-07-09 21:34:41 +0900

    feat: Set NullHandler to pacakge root logger

--- src/majsoulrpa/__init__.py
+++ src/majsoulrpa/__init__.py
@@ -1,5 +1,9 @@
+from logging import NullHandler, getLogger
+
 from majsoulrpa.app import RPAApp
 from majsoulrpa.config import AppConfig
+
+getLogger(__name__).addHandler(NullHandler())
 
 __version__ = "0.1.0"
 
